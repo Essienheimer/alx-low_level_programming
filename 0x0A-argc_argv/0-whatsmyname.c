f8d31dd50c716b2bdf200a709709a14282bd1c69
chibuike2010 <chibuike.sunday67@gmail>
2023-03-14 06:58:19 -0700

    this program prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*@argc: function count argument
+*@argv: function count vector
+*
+*Return: 0 success
+*/
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}

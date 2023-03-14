e67a167bff7615df645728a850b930f973708457
chibuike2010 <chibuike.sunday67@gmail>
2023-03-14 07:22:07 -0700

    this progam prints a number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - main function
+*@argc: argument count
+*@argv: argument vector
+*
+*Return: success 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc -1);
+	return (0);
+}
